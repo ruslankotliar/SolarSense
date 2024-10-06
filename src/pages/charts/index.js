5cc3f9bbeb9866fcc18e6cd1663bd05205da2e96
Ruslan Kotliarenko <ruslan.kotliarenko@dxc.com>
2024-10-06 11:30:47 +0200

    Layout on all pages

--- src/pages/charts/index.js
+++ src/pages/charts/index.js
@@ -1,6 +1,9 @@
 import { Line, Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { useEffect, useState } from 'react';
+import Layout from '@components/Layout';
+import Section from '@components/Section';
+import Container from '@components/Container';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarElement, Title, Tooltip, Legend);
 
@@ -111,131 +114,126 @@ export default function ChartPage() {
   }
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      gap: '20px',
-      width: '100%',
-      padding: '20px',
-      boxSizing: 'border-box',
-      height: '100vh',
-    }}>
-      <div style={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginBottom: '20px',
-        gap: '20px',
-      }}>
-        <h2 style={{
-          textAlign: 'center',
-          fontSize: '18px',
-          fontWeight: 'bold',
-        }}>UV Index and Skin Cancer Incidents <strong style={{color: '#1e3a8a'}}>Comparison</strong></h2>
+    <Layout>
+        <Section style={{ height: '100%' }}>
+        <Container style={{ height: '100%', margin: '0', width: '100%', maxWidth: '100%'}}>
+            <div style={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                marginBottom: '20px',
+                gap: '20px',
+            }}>
+                <h2 style={{
+                textAlign: 'center',
+                fontSize: '18px',
+                fontWeight: 'bold',
+                }}>UV Index and Skin Cancer Incidents <strong style={{color: '#1e3a8a'}}>Comparison</strong></h2>
 
-        <div style={{
-          borderRadius: '8px',
-          background: '#f0f4f8',
-          padding: '10px 20px',
-          boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
-          display: 'flex',
-          alignItems: 'center',
-        }}>
-          <label htmlFor="country-select" style={{
-            marginRight: '10px',
-            fontSize: '16px',
-            fontWeight: 'bold',
-          }}>Select a country:</label>
-          <select
-            id="country-select"
-            value={selectedCountry}
-            onChange={(e) => setSelectedCountry(e.target.value)}
-            style={{
-              padding: '8px',
-              borderRadius: '4px',
-              border: '1px solid #ccc',
-              backgroundColor: '#fff',
-              fontSize: '14px',
-            }}
-          >
-            {availableCountries.map((country) => (
-              <option key={country} value={country}>
-                {country}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
+                <div style={{
+                borderRadius: '8px',
+                background: '#f0f4f8',
+                padding: '10px 20px',
+                boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+                display: 'flex',
+                alignItems: 'center',
+                }}>
+                <label htmlFor="country-select" style={{
+                    marginRight: '10px',
+                    fontSize: '16px',
+                    fontWeight: 'bold',
+                }}>Select a country:</label>
+                <select
+                    id="country-select"
+                    value={selectedCountry}
+                    onChange={(e) => setSelectedCountry(e.target.value)}
+                    style={{
+                    padding: '8px',
+                    borderRadius: '4px',
+                    border: '1px solid #ccc',
+                    backgroundColor: '#fff',
+                    fontSize: '14px',
+                    }}
+                >
+                    {availableCountries.map((country) => (
+                    <option key={country} value={country}>
+                        {country}
+                    </option>
+                    ))}
+                </select>
+                </div>
+            </div>
 
-      <div style={{
-        display: 'flex',
-        gap: '20px',
-        justifyContent: 'center',
-        flexWrap: 'wrap',
-        width: '100%',
-        height: '50%',
-      }}>
-        <div style={{
-          flex: '1 1 300px', // Allows the chart to be responsive and grow/shrink
-          background: '#fff',
-          padding: '20px',
-          borderRadius: '8px',
-          boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
-        }}>
-          {/* Line Chart */}
-          <Line
-            data={chartData}
-            options={{
-              responsive: true,
-              maintainAspectRatio: false, // Allows for better responsiveness
-              scales: {
-                y1: {
-                  type: 'linear',
-                  position: 'left',
-                  title: { display: true, text: 'Avg UV' },
-                },
-                y2: {
-                  type: 'linear',
-                  position: 'right',
-                  title: { display: true, text: 'Cases' },
-                },
-              },
-            }}
-          />
-        </div>
+            <div style={{
+                display: 'flex',
+                gap: '20px',
+                justifyContent: 'center',
+                flexWrap: 'wrap',
+                width: '100%',
+                height: '50%',
+            }}>
+                <div style={{
+                flex: '1 1 300px', // Allows the chart to be responsive and grow/shrink
+                background: '#fff',
+                padding: '20px',
+                borderRadius: '8px',
+                boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+                }}>
+                {/* Line Chart */}
+                <Line
+                    data={chartData}
+                    options={{
+                    responsive: true,
+                    maintainAspectRatio: false, // Allows for better responsiveness
+                    scales: {
+                        y1: {
+                        type: 'linear',
+                        position: 'left',
+                        title: { display: true, text: 'Avg UV' },
+                        },
+                        y2: {
+                        type: 'linear',
+                        position: 'right',
+                        title: { display: true, text: 'Cases' },
+                        },
+                    },
+                    }}
+                />
+                </div>
 
-        <div style={{
-          flex: '1 1 300px', // Allows the chart to be responsive and grow/shrink
-          background: '#fff',
-          padding: '20px',
-          borderRadius: '8px',
-          boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
-        }}>
-          {/* Bar Chart */}
-          <Bar
-            data={barChartData}
-            options={{
-              responsive: true,
-              maintainAspectRatio: false, // Allows for better responsiveness
-              scales: {
-                y1: {
-                  type: 'linear',
-                  position: 'left',
-                  title: { display: true, text: 'Avg UV' },
-                  grid: { drawOnChartArea: false },
-                },
-                y2: {
-                  type: 'linear',
-                  position: 'right',
-                  title: { display: true, text: 'Cases' },
-                },
-              },
-            }}
-          />
-        </div>
-      </div>
-    </div>
+                <div style={{
+                flex: '1 1 300px', // Allows the chart to be responsive and grow/shrink
+                background: '#fff',
+                padding: '20px',
+                borderRadius: '8px',
+                boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+                }}>
+                {/* Bar Chart */}
+                <Bar
+                    data={barChartData}
+                    options={{
+                    responsive: true,
+                    maintainAspectRatio: false, // Allows for better responsiveness
+                    scales: {
+                        y1: {
+                        type: 'linear',
+                        position: 'left',
+                        title: { display: true, text: 'Avg UV' },
+                        grid: { drawOnChartArea: false },
+                        },
+                        y2: {
+                        type: 'linear',
+                        position: 'right',
+                        title: { display: true, text: 'Cases' },
+                        },
+                    },
+                    }}
+                />
+                </div>
+            </div>
+        </Container>
+        </Section>
+    </Layout>
   );
 }
